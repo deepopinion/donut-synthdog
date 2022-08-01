838c1cb131ce2372a9f40e7d024b36b58e408072
Philemon Schöpf <philemon.schoepf@deepopinion.ai>
2022-08-01 06:50:06 +0200

    copy apply function from perspective

--- synthdog/elements/document.py
+++ synthdog/elements/document.py
@@ -9,6 +9,8 @@ from synthtiger import components
 from elements.content import Content
 from elements.paper import Paper
 
+from synthtiger.layers import Group
+import cv2
 
 class Document:
     def __init__(self, config):
@@ -61,5 +63,51 @@ class Document:
         text_layers, texts, word_quads = self.content.generate(size)
         paper_layer = self.paper.generate(size)
         self.effect.apply([*text_layers, paper_layer])
+
+        def apply_to_quads(quads, layers, meta=None):
+            """
+            Adapted from synthtiger.components.Perspective().apply.
+
+            Use the meta dict given back by the effect function to calculate the effect on the word bounding quads.
+            """
+
+            meta = self.sample(meta)
+            pxs = meta["pxs"]
+            percents = meta["percents"]
+            aligns = meta["aligns"]
+
+            aligns = np.tile(aligns, 4)[:4]
+            if pxs is not None:
+                pxs = np.tile(pxs, 4)[:4]
+            if percents is not None:
+                percents = np.tile(percents, 4)[:4]
+
+            group = Group(layers)
+            sizes = np.tile(group.size, 2)
+            new_sizes = np.tile(group.size, 2)
+
+            if pxs is not None:
+                new_sizes += pxs
+            elif percents is not None:
+                new_sizes *= percents
+
+            values = (sizes - new_sizes) / 2
+            aligns *= np.abs(values)
+            offsets = [
+                [values[0] + aligns[0], values[3] + aligns[3]],
+                [-values[0] + aligns[0], values[1] + aligns[1]],
+                [-values[2] + aligns[2], -values[1] + aligns[1]],
+                [values[2] + aligns[2], -values[3] + aligns[3]],
+            ]
+
+            origin = group.quad
+            quad = np.array(origin + offsets, dtype=np.float32)
+            matrix = cv2.getPerspectiveTransform(origin, quad)
+
+            for layer in layers:
+                quad = np.append(layer.quad, np.ones((4, 1)), axis=-1).dot(matrix.T)
+                layer.quad = quad[..., :2] / quad[..., 2, np.newaxis]
+
+            return meta
 
         return paper_layer, text_layers, texts, word_quads
