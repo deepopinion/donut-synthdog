f83193494e550aae1bbd9c866879e6233aee8031
Philemon Schöpf <philemon.schoepf@deepopinion.ai>
2022-08-01 18:50:27 +0200

    clean up perspective transform
    matrix now comes directly from SynthTIGER apply, no recalculation

--- synthdog/elements/document.py
+++ synthdog/elements/document.py
@@ -63,53 +63,11 @@ class Document:
         text_layers, texts, word_quads = self.content.generate(size)
         paper_layer = self.paper.generate(size)
         meta = self.effect.apply([*text_layers, paper_layer])
+        matrix = meta['metas'][2]['meta']['meta']['matrix']
 
-        def apply_to_quads(quads, layers, meta=None):
-            """
-            Adapted from synthtiger.components.Perspective().apply.
-
-            Use the meta dict given back by the effect function to calculate the effect on the word bounding quads.
-            """
-
-            pxs = meta["pxs"]
-            percents = meta["percents"]
-            aligns = meta["aligns"]
-
-            aligns = np.tile(aligns, 4)[:4]
-            if pxs is not None:
-                pxs = np.tile(pxs, 4)[:4]
-            if percents is not None:
-                percents = np.tile(percents, 4)[:4]
-
-            group = Group(layers)
-            sizes = np.tile(group.size, 2)
-            new_sizes = np.tile(group.size, 2)
-
-            if pxs is not None:
-                new_sizes += pxs
-            elif percents is not None:
-                new_sizes *= percents
-
-            values = (sizes - new_sizes) / 2
-            aligns *= np.abs(values)
-            offsets = [
-                [values[0] + aligns[0], values[3] + aligns[3]],
-                [-values[0] + aligns[0], values[1] + aligns[1]],
-                [-values[2] + aligns[2], -values[1] + aligns[1]],
-                [values[2] + aligns[2], -values[3] + aligns[3]],
-            ]
-
-            origin = group.quad
-            quad = np.array(origin + offsets, dtype=np.float32)
-            matrix = cv2.getPerspectiveTransform(origin, quad)
-
-            transformed_quads = []
-            for q in quads:
-                q = np.append(q, np.ones((4, 1)), axis=-1).dot(matrix.T)
-                transformed_quads.append(q[..., :2] / q[..., 2, np.newaxis])
-
-            return transformed_quads
-
-        transformed_quads = apply_to_quads(word_quads, [*text_layers, paper_layer], meta=meta['metas'][2]['meta']['meta'])
+        transformed_quads = []
+        for word_quad in word_quads:
+            quad = np.append(word_quad, np.ones((4, 1)), axis=-1).dot(matrix.T)
+            transformed_quads.append(quad[..., :2] / quad[..., 2, np.newaxis])
 
         return paper_layer, text_layers, texts, transformed_quads
