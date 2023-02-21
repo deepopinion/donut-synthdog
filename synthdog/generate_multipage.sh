efdc33025830608faa269a52735f207818685299
Philemon Schöpf <philemon.schoepf@deepopinion.ai>
2023-02-21 17:56:24 +0100

    convert subsests

--- synthdog/generate_multipage.sh
+++ synthdog/generate_multipage.sh
@@ -2,10 +2,14 @@
 
 # One-line script to generate a SynthDog dataset with multiple types of docs
 # in Autotransformers format.
+
+AMOUNT=${1:-20}
+WORKERS=${2:-4}
+
 cd ~/data/ocr/donut/synthdog
 rm ~/data/autotransformers/.data/document/multipage_synthetic -r
-synthtiger -o ~/data/autotransformers/.data/document/multipage_synthetic/text -c 20 -w 4 -v template.py SynthDoG config_en_clean.yml 
-synthtiger -o ~/data/autotransformers/.data/document/multipage_synthetic/numbers -c 20 -w 4 -v template.py SynthDoG config_en_numbers.yml 
-synthtiger -o ~/data/autotransformers/.data/document/multipage_synthetic/sparse -c 20 -w 4 -v template.py SynthDoG config_en_sparse.yml
+synthtiger -o ~/data/autotransformers/.data/document/multipage_synthetic/text -c $AMOUNT -w $WORKERS template.py SynthDoG config_en_clean.yml 
+synthtiger -o ~/data/autotransformers/.data/document/multipage_synthetic/numbers -c $AMOUNT -w $WORKERS template.py SynthDoG config_en_numbers.yml 
+synthtiger -o ~/data/autotransformers/.data/document/multipage_synthetic/sparse -c $AMOUNT -w $WORKERS template.py SynthDoG config_en_sparse.yml
 
 python3 utils/metadata_to_dataset.py ~/data/autotransformers/.data/document/multipage_synthetic
--- synthdog/utils/metadata_to_dataset.py
+++ synthdog/utils/metadata_to_dataset.py
@@ -15,11 +15,14 @@ parser.add_argument("input_folder", type=str, help="Path to the folder containin
 
 args = parser.parse_args()
 logger.info("Converting folder %s", args.input_folder)
-samples = []
+samples = {"train": [], "eval": [], "test": []}
 for dirpath, _, filenames in os.walk(args.input_folder):
     logger.info("Processing subfolder %s", dirpath)
     if not "metadata.jsonl" in filenames:
         continue
+    split = os.path.split(dirpath)[1]  # can be "train", "eval" or "test"
+    if split == "validation":
+        split = "eval"  # rename "validation" to "eval"
     # each folder with a metadata.jsonl file is added to the dataset
     with jsonlines.open(os.path.join(dirpath, "metadata.jsonl"), mode="r") as reader:
         for line in reader:
@@ -31,7 +34,7 @@ for dirpath, _, filenames in os.walk(args.input_folder):
             boxes = bboxes = gt["quads"]
             doc["bboxes"] = [BBox.from_easy_ocr_output(box_as_quads).to_dict() for box_as_quads in boxes]
 
-            samples.append(doc)
+            samples[split].append(doc)
 
 with open(os.path.join(args.input_folder, "dataset.json"), mode="w") as f:
     json.dump(samples, f, indent=4)
